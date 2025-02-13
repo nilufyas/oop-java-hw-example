116d3ee4910befd313b58bc90b9e3848810b55b0
Dmitry Sayfulin <sayfulindmitry@gmail.com>
2025-02-13 12:41:14 +0300

    add homework example

---
+++ src/Enemy.java
@@ -0,0 +1,36 @@
+/**
+ * Класс Enemy (Враг)
+ *
+ * ОПИСАНИЕ НА РУССКОМ:
+ * 1) В этом классе нужно объявить поля (например, name, health, attackPower).
+ * 2) Реализовать конструктор(ы) для инициализации полей.
+ * 3) Написать методы:
+ *    - attack(Player player): Логика атаки игрока.
+ *    - takeDamage(int damage): Уменьшает здоровье врага.
+ *    - isAlive(): Возвращает true, если здоровье > 0.
+ * 4) Опционально: добавить геттеры/сеттеры, если нужно.
+ */
+public class Enemy {
+    // private String name;
+    // private int health;
+    // private int attackPower;
+
+    // public Enemy(String name, int health, int attackPower) {
+    //     // TODO: Инициализировать поля
+    // }
+
+    // public void attack(Player player) {
+    //     // TODO: Логика нанесения урона игроку
+    // }
+
+    // public void takeDamage(int damage) {
+    //     // TODO: Логика уменьшения здоровья
+    // }
+
+    // public boolean isAlive() {
+    //     // TODO: Вернуть true, если здоровье врага больше 0
+    //     // return false; // заглушка
+    // }
+
+    // Optional: добавить любые другие методы, если требуется (например, getName(), getHealth(), и т.д.)
+}
---
+++ src/Player.java
@@ -0,0 +1,36 @@
+/**
+ * Класс Player (Игрок)
+ *
+ * ОПИСАНИЕ НА РУССКОМ:
+ * 1) В этом классе нужно объявить поля (например, name, health, attackPower).
+ * 2) Реализовать конструктор(ы) для инициализации полей.
+ * 3) Написать методы:
+ *    - attack(Enemy enemy): Логика атаки врага.
+ *    - takeDamage(int damage): Уменьшает здоровье игрока.
+ *    - isAlive(): Возвращает true, если здоровье > 0.
+ * 4) Опционально: добавить геттеры/сеттеры, метод heal() или другие методы по необходимости.
+ */
+public class Player {
+    // private String name;
+    // private int health;
+    // private int attackPower;
+
+    // public Player(String name, int health, int attackPower) {
+    //     // TODO: Инициализировать поля
+    // }
+
+    // public void attack(Enemy enemy) {
+    //     // TODO: Логика нанесения урона enemy
+    // }
+
+    // public void takeDamage(int damage) {
+    //     // TODO: Логика уменьшения здоровья
+    // }
+
+    // public boolean isAlive() {
+    //     // TODO: Вернуть true, если здоровье игрока больше 0
+    //     // return false; // заглушка
+    // }
+
+    // Optional: добавить любые другие методы по желанию (например, heal(), getName(), getHealth() и т.д.)
+}
